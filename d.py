a95f519594b2a112fd93fe06cba897ce8717cf52
Andrew Magee <amagee@gmail.com>
2020-09-06 13:53:50 +1000

    it doesn't do more stuff but it is longer

--- d.py
+++ d.py
@@ -3,8 +3,13 @@ import random
 from bs4 import BeautifulSoup
 import time
 import math
+from wsgiref import simple_server
+from threading import Thread
+from telnetlib import Telnet
 
 
+# We want to print "Hello, World!". So naturally we start by creating a mapping
+# of characters to the set of indices in a list where the character appears.
 d = {
     '_': [
         1, 5, 12, 14, 43, 49, 51, 111, 116, 117, 118, 125, 126, 127, 131,
@@ -32,30 +37,78 @@ d = {
 }
 
 
+# Now it's a simple matter of unpacking the dictionary into a list of lines we
+# can print (after a suitable amount of processing, of course).
 l = [" "] * 323
-
 for k, v in d.items():
     for i in v:
         l[i] = k
-
 s = "".join(l)
-
 lines = s.split("\n")
+
 max_line_length = max([len(l) for l in lines])
 
+# HTML is the language of the future, so we future-proof our program by writing
+# the data in HTML.
 sio = StringIO()
-
 for line in lines:
     sio.write('<div class="line">')
     for char in line:
         sio.write(f'<span data-char="{char}" />')
     sio.write('</div>')
 
+# But we want to make sure the output is pretty, so we use BeatifulSoup to
+# prettify the HTML.
 soup = BeautifulSoup(sio.getvalue(), 'html.parser')
-
 pretty = soup.prettify()
 
 
+# Remember though that HTML can only exist if we serve it from a web server, so
+# let's make one of those.
+def application(environ, start_response):
+    status = '200 OK'
+    headers = [('Content-type', 'text/html; charset=utf-8')]
+    start_response(status, headers)
+    return [pretty.encode()]
+
+server = simple_server.make_server("localhost", 8080, application)
+thread = Thread(target=server.serve_forever)
+thread.start()
+
+# The `requests` module is a bit mainstream, and importing it is too much
+# effort (as is figuring out how to use most of the non-requests ways of making
+# HTTP requests). So use telnet to retrieve the data from our web server.
+tn = Telnet("localhost", 8080)
+tn.write("GET / HTTP/1.1\r\nHOST: localhost\r\n\r\n".encode())
+response_lines = tn.read_all().decode().split("\n")
+blank_index = response_lines.index("\r")
+response_content = "\n".join(response_lines[blank_index + 1:])
+
+server.shutdown()
+thread.join()
+
+# Now we've retrieved the pretty HTML, we can process it into a list of
+# (line_index, character_index, character) tuples.
+characters = []
+
+line_index = 0
+char_index = 0
+for line in response_content.split("\n"):
+    if line.startswith("</div>"):
+        line_index += 1
+        char_index = 0
+    elif "data-char" in line:
+        char = line[line.index('"') + 1]
+        characters.append((line_index, char_index, char))
+        char_index += 1
+
+# We want the characters to appear in a random order.
+random.shuffle(characters)
+
+
+# Now we have sufficiently processed the data, let's write "Hello world!" to
+# the screen.  We'll write it first using the curlyboi colours, and then some
+# random colours, and we'll repeat that process 10 times.
 bold = "\x1b[1m";
 
 curlyboi_colours = [
@@ -75,21 +128,7 @@ rand_colours = [
     '\033[91m',
 ]
                            
-
-characters = []
-
-line_index = 0
-char_index = 0
-for line in pretty.split("\n"):
-    if line.startswith("</div>"):
-        line_index += 1
-        char_index = 0
-    elif "data-char" in line:
-        char = line[line.index('"') + 1]
-        characters.append((line_index, char_index, char))
-        char_index += 1
-
-
+# Don't forget to do some software engineering.
 class CurlyBoiColourGetter:
     def __init__(self, num_curlybois):
         self.curlybois = curlyboi_colours * num_curlybois
@@ -100,9 +139,6 @@ class CurlyBoiColourGetter:
 
 def get_colour_2(line_index, char_index, char):
     return random.choice(rand_colours)
-
-
-random.shuffle(characters)
 
 print("\n" * 10)
 print("\033[s", end="") # save
@@ -124,4 +160,3 @@ for i in range(0, 10):
         time.sleep(0.5)
 
 print("\n" * 7)
-
